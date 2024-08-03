ea55e5cc6b111fcb3dbe182a86fceffa74c09d00
JoelTatso <sietchomzeutatsoj@gmail.com>
2024-08-03 03:35:37 +0200

    configuration angularFire : leMarcheAdmin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { AngularFireModule } from '@angular/fire/compat';
+import { collection, collectionData, enableIndexedDbPersistence, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, AngularFireModule],
+  template: `
+
+      <h1 align="center">Bienvenue sur le marche Admin</h1>
+
+      <ul>
+        @for (item of items(); track $index) {
+          <li>{{ item?.name }}</li>
+        }
+      </ul>
+
+      <router-outlet />
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng_18';
+
+  firestore : Firestore = inject(Firestore)
+  items : Signal<any[] | undefined>
+
+  constructor(){
+    enableIndexedDbPersistence(this.firestore)
+      const Collection = collection(this.firestore,'items')
+      this.items = toSignal(collectionData(Collection))
+
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,28 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, persistentLocalCache, persistentMultipleTabManager, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [  provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+                provideFirebaseApp(() => initializeApp({
+                  "projectId":"angularfire-72f62",
+                  "appId":"1:326245839841:web:eca823eba3c04a2c4f58a0",
+                  "storageBucket":"angularfire-72f62.appspot.com",
+                  "apiKey":"AIzaSyDlpuF5-ANVEtx_ieGA0uv3K4rfKeYJt1g",
+                  "authDomain":"angularfire-72f62.firebaseapp.com",
+                  "messagingSenderId":"326245839841"})),
+
+                provideAuth(() => getAuth()),
+                provideFirestore(() => getFirestore()/* ,{
+                  localCache: persistentLocalCache({
+                    tabManager: persistentMultipleTabManager()
+                  })
+                } */),
+                provideStorage(() => getStorage()), provideAnimationsAsync()
+              ]
 };
